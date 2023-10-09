75475d15f84e8cf3a8ffc168f2fc4c4b91763b67
Alex Ulanovich <ulaalex@mail.ru>
2023-10-09 21:51:42 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', './app.component.adaptive.css']
 })
 export class AppComponent {
   title = 'my-app';
