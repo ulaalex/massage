bdf373f678b796c35bb1e7a81cde379585a16343
Alex Ulanovich <ulaalex@mail.ru>
2023-10-11 22:53:59 +0300

    11.10.2023_22.53

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css', './app.component.adaptive.css']
 })
 export class AppComponent {
-  title = 'my-app';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
   ],
   imports: [
     BrowserModule,
