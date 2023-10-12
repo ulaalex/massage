f90311bc84533a91684f9e880d102891da4727cd
Alex Ulanovich <ulaalex@mail.ru>
2023-10-12 22:41:51 +0300

    12.10.2023_22.41

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { NotFoundComponent } from './not-found.component';
 
+
 const routes: Routes = [
-  { path: '', component: HomeComponent},
-  { path: '**', component: NotFoundComponent}
+  { path: '', component: HomeComponent, },
+  { path: '**', component: NotFoundComponent, }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HomeComponent } from './home.component';
+import { NotFoundComponent } from './not-found.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    NotFoundComponent,    
   ],
   imports: [
     BrowserModule,
