24737756ddf4d0ec12c5bbcbbee89909a84af89c
Alex Ulanovich <ulaalex@mail.ru>
2024-04-13 14:19:53 +0300

    13.04.2024_14.19

--- src/app/chat.component.ts
+++ src/app/chat.component.ts
@@ -20,13 +20,23 @@ import { ChatMobileComponent } from './chat-mobile.component';
 })
 export class ChatComponent implements AfterViewInit {
 
+
+    maxMobileWidthPx = 600;
+    
+    isMobile = () => {
+        return /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent);
+    }
+
     @ViewChild('chat', { read: ViewContainerRef })
     private viewRef!: ViewContainerRef;
     private componentRef!: ComponentRef<ChatMobileComponent>;
 
     showDynamicComponent(): void {
-        this.viewRef.clear();
-        this.componentRef = this.viewRef.createComponent(ChatMobileComponent);
+        console.log(window.navigator);
+        if (!this.isMobile()) {
+            this.viewRef.clear();
+            this.componentRef = this.viewRef.createComponent(ChatMobileComponent);
+        }
     }
 
     removeDynamicComponent(): void {
