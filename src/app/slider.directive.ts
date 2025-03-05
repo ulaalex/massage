73c5430951eb2d955d5ae8fdcc086e6868323629
Alex Ulanovich <ulaalex@mail.ru>
2025-03-05 23:06:31 +0300

    05.03.2025_23.06

--- src/app/slider.directive.ts
+++ src/app/slider.directive.ts
@@ -13,7 +13,7 @@ export class SliderDirective implements AfterViewInit {
     createSlider(element: { querySelector: (arg0: string) => any; querySelectorAll: (arg0: string) => Iterable<unknown> | ArrayLike<unknown>; }) {
 
         const slider = element.querySelector('.slider');
-        const slideDiv = element.querySelector('.slide');
+        // const slideDiv = element.querySelector('.slide');
         const prevButton = element.querySelector('.prev-button');
         const nextButton = element.querySelector('.next-button');
         const slides = Array.from(element.querySelectorAll('img'));
@@ -31,21 +31,21 @@ export class SliderDirective implements AfterViewInit {
             slide();
         });
 
-        if (slideDiv) {
-            const ro = new ResizeObserver(entries => {
-                for (let entry of entries) {
-                    if (entry.target === slideDiv) {
-                        slide();
-                    }
-                }
-            });
-            ro.observe(slideDiv);
-        }
+        // if (slideDiv) {
+        //     const ro = new ResizeObserver(entries => {
+        //         for (let entry of entries) {
+        //             if (entry.target === slideDiv) {
+        //                 slide();
+        //             }
+        //         }
+        //     });
+        //     ro.observe(slideDiv);
+        // }
 
         function slide() {
-            const imageWidth = slider.clientWidth;
-            const slideOffset = -slideIndex * imageWidth;
-            slider.style.transform = `translateX(${slideOffset}px)`;
+            // const imageWidth = slider.clientWidth;
+            const slideOffset = -slideIndex * 100;
+            slider.style.transform = `translateX(${slideOffset}%)`;
         }
 
     }
